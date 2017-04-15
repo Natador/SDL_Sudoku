d10eeb488c65fe04bb4d31275335424e2de564d9
nat <nadacant@iupui.edu>
2017-04-15 12:45:39 -0400

    Init function added to main C file

--- main.c
+++ main.c
@@ -6,15 +6,31 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 #define ROWS 9
 #define COLS 9
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+//Global variables to be universally accessed
+SDL_Window* myWindow = NULL;	//The window we'll use
+SDL_Renderer* myRender = NULL;	//The window renderer
 
 void printBoard(int board[][COLS]);
+bool initSDL();
 
 int main(int argv, char*args[]) {
     int mainboard[9][9] = {0};
+	int x, y, n, count = 0;
     printBoard(mainboard);
+	while (1) {
+		printf("Please enter a move\n");
+		if(scanf("%d %d %d", &x, &y, &n) != 3)
+				break;
+		mainboard[x-1][y-1] = n;
+		printBoard(mainboard);
+	}
     return 0;
 }
 
@@ -35,4 +51,36 @@ void printBoard(int board[][COLS]) {
         }
         printf("\n");
     }
+}
+
+//Initializes SDL and crap
+bool id initSDL() {
+	bool success = true; //Init flag
+
+	//Initalize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else {
+		//Create the window
+		myWindow = SDL_CreateWindow( "Sudoku Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+		if (myWindow == NULL) {
+			printf("Window couldn't be created! SDL error: %s", SDL_GetError());
+			success = false;
+		}
+		else {
+			//Make the window renderer
+			myRender = SDL_CreateRenderer(myWindow, -1, SDL_RENDER_ACCELERATED);
+			if (myRender == NULL) {
+				printf("Renderer could not be created! SDL Error: %s", SDL_GetError());
+				success = false;
+			}
+			else {
+				//Initialize the renderer color
+				SDL_SetRenderDrawColor(myRender, 0xFF, 0xFF, 0xFF, 0xFF)
+			}
+		}
+	}
 }
